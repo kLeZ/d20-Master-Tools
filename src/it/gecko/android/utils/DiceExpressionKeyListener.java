6f4f2f3e3cd3f4eedf067833c69a5cf332b8dae3
Alessandro Accardo <julius8774@gmail.com>
2012-08-21 09:40:42 +0200

    Updated DiceExpressionKeyListener.

--- src/it/gecko/android/utils/DiceExpressionKeyListener.java
+++ src/it/gecko/android/utils/DiceExpressionKeyListener.java
@@ -2,35 +2,35 @@ package it.gecko.android.utils;
 
 import it.gecko.utils.Dice;
 import it.gecko.utils.OperatorType;
-
-import org.apache.commons.lang3.ArrayUtils;
-
-import android.text.InputType;
-import android.text.Spanned;
-import android.text.method.NumberKeyListener;
+import android.text.*;
+import android.text.method.BaseKeyListener;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-public class DiceExpressionKeyListener extends NumberKeyListener
+public class DiceExpressionKeyListener extends BaseKeyListener implements InputFilter
 {
 	private static final char[] BASE_CHARS = new char[]
 	{ Dice.DICE_TOKEN, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-	public static final char[] CHARACTERS = ArrayUtils.addAll(BASE_CHARS, OperatorType.getOperators());
+	private static final String TAG = "it.gecko.android.utils.DiceExpressionKeyListener";
 
 	private final char[] accepted;
 
-	private TextView textView;
-	private CheckBox debugCheck;
+	private final TextView textView;
+	private final CheckBox debugCheck;
 
 	public DiceExpressionKeyListener(TextView textView, CheckBox debugCheck)
 	{
 		super();
 		this.textView = textView;
 		this.debugCheck = debugCheck;
-		accepted = CHARACTERS;
+		accepted = new char[BASE_CHARS.length + OperatorType.getOperators().length];
+		System.arraycopy(BASE_CHARS, 0, accepted, 0, BASE_CHARS.length);
+		System.arraycopy(OperatorType.getOperators(), 0, accepted, BASE_CHARS.length, OperatorType.getOperators().length);
 	}
 
-	@Override
 	public char[] getAcceptedChars()
 	{
 		return accepted;
@@ -45,7 +45,7 @@ public class DiceExpressionKeyListener extends NumberKeyListener
 		return InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS;
 	}
 
-	private boolean isDebug()
+	public boolean isDebug()
 	{
 		return debugCheck.isChecked();
 	}
@@ -88,49 +88,121 @@ public class DiceExpressionKeyListener extends NumberKeyListener
 	@Override
 	public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend)
 	{
-		CharSequence ret = super.filter(source, start, end, dest, dstart, dend);
-		if (isDebug())
+		@SuppressWarnings("unused")
+		boolean filter = false;
+
+		int i;
+		for (i = start; i < end; i++)
 		{
-			textView.append(printFilter(source, start, end, dest, dstart, dend, ret));
+			if (!ok(getAcceptedChars(), source.charAt(i)))
+			{
+				break;
+			}
+		}
+
+		if (i == end)
+		{
+			// It was all OK.
+			return null;
+		}
+
+		if ((end - start) == 1)
+		{
+			// It was not OK, and there is only one char, so nothing remains.
+			return "";
+		}
+
+		SpannableStringBuilder filtered = new SpannableStringBuilder(source, start, end);
+		i -= start;
+		end -= start;
+
+		@SuppressWarnings("unused")
+		int len = end - start;
+		// Only count down to i because the chars before that were all OK.
+		for (int j = end - 1; j >= i; j--)
+		{
+			if (!ok(getAcceptedChars(), source.charAt(j)))
+			{
+				filtered.delete(j, j + 1);
+			}
+		}
+
+		/*
+		 * START OF SELF CODE
+		 */
+
+		log(printFilter(source, start, end, dest, dstart, dend, filtered));
+		if (filtered != null)
+		{
+			source = filtered;
+			start = 0;
+			end = filtered.length();
 		}
 
 		if (source.length() > 0)
 		{
-			char c = source.charAt(0);
+			log("I'm in -> source.length() > 0");
+			char c = last(source);
 			String sdest = dest.toString();
 			if (sdest.isEmpty() && !Character.isDigit(c))
 			{
-				deleteFilter(source, start, end, dest, dstart, dend, ret);
+				log("I'm in -> sdest.isEmpty() && !Character.isDigit(c)");
+				deleteFilter(source, start, end, dest, dstart, dend, filtered);
 			}
 			else if (c == Dice.DICE_TOKEN)
 			{
+				log("I'm in -> c == Dice.DICE_TOKEN");
 				if (!Character.isDigit(last(sdest)) || sdest.contains(Dice.DICE_TOKEN_S))
 				{
-					deleteFilter(source, start, end, dest, dstart, dend, ret);
+					log("I'm in -> !Character.isDigit(last(sdest)) || sdest.contains(Dice.DICE_TOKEN_S)");
+					deleteFilter(source, start, end, dest, dstart, dend, filtered);
 				}
 			}
 			else if (OperatorType.isOperator(c))
 			{
+				log("I'm in -> OperatorType.isOperator(c)");
 				if (!Character.isDigit(last(sdest)) || OperatorType.containsOperator(sdest))
 				{
-					deleteFilter(source, start, end, dest, dstart, dend, ret);
+					log("I'm in -> !Character.isDigit(last(sdest)) || OperatorType.containsOperator(sdest)");
+					deleteFilter(source, start, end, dest, dstart, dend, filtered);
 				}
 			}
 		}
-		return ret;
+		return filtered;
 	}
 
-	private char last(String sp)
+	protected static boolean ok(char[] accept, char c)
 	{
-		return sp.charAt(sp.length() - 1);
+		for (int i = accept.length - 1; i >= 0; i--)
+		{
+			if (accept[i] == c) { return true; }
+		}
+
+		return false;
+	}
+
+	@SuppressWarnings("unused")
+	private char last(Spanned s)
+	{
+		return s.charAt(s.length() - 1);
+	}
+
+	private char last(CharSequence s)
+	{
+		return s.charAt(s.length() - 1);
+	}
+
+	private char last(String s)
+	{
+		return s.charAt(s.length() - 1);
 	}
 
 	private void deleteFilter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend, CharSequence ret)
 	{
-		ret = "";
+		ret = null;
 		source = "";
 		start = 0;
-		end = 0;
+		end = Math.max(source.length(), dest.length());
 		dstart = 0;
 		dend = dest.length();
 	}
@@ -149,5 +221,72 @@ public class DiceExpressionKeyListener extends NumberKeyListener
 		sb.append("]");
 		sb.append(System.getProperty("line.separator"));
 		return sb.toString();
+	}
+
+	private void log(String text)
+	{
+		if (isDebug())
+		{
+			textView.append(text);
+			textView.append(System.getProperty("line.separator"));
+			Log.d(TAG, text);
+		}
+	}
+
+	protected int lookup(KeyEvent event, Spannable content)
+	{
+		return event.getMatch(getAcceptedChars(), event.getMetaState() | getMetaState(content));
+	}
+
+	@Override
+	public boolean onKeyDown(View view, Editable content, int keyCode, KeyEvent event)
+	{
+		int selStart, selEnd;
+
+		{
+			int a = Selection.getSelectionStart(content);
+			int b = Selection.getSelectionEnd(content);
+
+			selStart = Math.min(a, b);
+			selEnd = Math.max(a, b);
+		}
+
+		if ((selStart < 0) || (selEnd < 0))
+		{
+			selStart = selEnd = 0;
+			Selection.setSelection(content, 0);
+		}
+
+		int i = event != null ? lookup(event, content) : 0;
+		int repeatCount = event != null ? event.getRepeatCount() : 0;
+		if (repeatCount == 0)
+		{
+			if (i != 0)
+			{
+				if (selStart != selEnd)
+				{
+					Selection.setSelection(content, selEnd);
+				}
+
+				content.replace(selStart, selEnd, String.valueOf((char) i));
+
+				adjustMetaAfterKeypress(content);
+				return true;
+			}
+		}
+		else if ((i == '0') && (repeatCount == 1))
+		{
+			// Pretty hackish, it replaces the 0 with the +
+
+			if ((selStart == selEnd) && (selEnd > 0) && (content.charAt(selStart - 1) == '0'))
+			{
+				content.replace(selStart - 1, selEnd, String.valueOf('+'));
+				adjustMetaAfterKeypress(content);
+				return true;
+			}
+		}
+
+		adjustMetaAfterKeypress(content);
+		return super.onKeyDown(view, content, keyCode, event);
 	}
 }
